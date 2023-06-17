259962cce0b608ce72869ffc698738a7695e3838
EttakiA <ahmedettaki1@gmail.com>
2023-06-17 11:11:42 -0700

    ahmed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+nclude <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A C progam that printswith printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("th proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
