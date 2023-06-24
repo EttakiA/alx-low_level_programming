1cdd22b2b3bd393d8f475935d87e2f6b841ad5bf
EttakiA <ahmedettaki1@gmail.com>
2023-06-24 13:40:30 -0700

    ahmed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * nul - multiplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+*/
+
+int nul(int a, int b).
+{
+	return (a * b);
+}
+
