161c364a42f03b09d0e23083a8a0a0c39a24da04
EttakiA <ahmedettaki1@gmail.com>
2023-06-28 08:23:08 -0700

    ahmed

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -19,7 +19,7 @@ int main(void)
 		putchar(digit + '0');
 		digit++;
 	}
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
